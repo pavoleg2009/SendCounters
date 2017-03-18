035eaa8fd0e25b8b3fd79090dc86e91d95a3cf4e
Oleg Pavlichenkov <pavoleg@mail.ru>
2017-03-18 18:24:57 +0300

    Facebook login commit

--- SendMeters/LoginController.m
+++ SendMeters/LoginController.m
@@ -19,6 +19,8 @@
 - (void) setLoginButtonsStack;
 - (void) setNativeFBLoginButton;
 - (void) setCustomFBLoginButton;
+
+- (void) loginWithFacebook;
 - (void) onFacebookLogin;
 
 @end
@@ -66,7 +68,7 @@
     loginButton.delegate = self;
     //FBSDKLoginButton *loginButton = [[FBSDKLoginButton alloc] initWithFrame:CGRectMake(0, 0, 50, 50)];
     
-    //loginButton.readPermissions = @[@"public_profile", @"email", @"user_friends"];
+    loginButton.readPermissions = @[@"public_profile", @"email"];
     [loginButton setTranslatesAutoresizingMaskIntoConstraints:NO];
     //[self.loginButtonsStack addSubview:loginButton];
     [self.loginButtonsStack addArrangedSubview:loginButton];
@@ -74,22 +76,40 @@
 //    [[loginButton.leftAnchor constraintEqualToAnchor:self.loginButtonsStack.leftAnchor constant:16]  setActive:YES];
 //    [[loginButton.topAnchor constraintEqualToAnchor:self.loginButtonsStack.topAnchor constant:40] setActive:YES];
 //    [[loginButton.rightAnchor constraintEqualToAnchor:self.loginButtonsStack.rightAnchor constant:-16] setActive:YES];
-    // FIXME: Height doesn't affected by heightAncho
- //   [[loginButton.heightAnchor constraintEqualToConstant:50] setActive:YES];
+//    [[loginButton.heightAnchor constraintEqualToConstant:50] setActive:YES];
     
     self.loginButton = loginButton;
 }
 
 - (void)setCustomFBLoginButton {
-    UIButton *customFBLoginButton = [[UIButton alloc] init];
+    UIButton *customFBLoginButton = [UIButton buttonWithType:UIButtonTypeSystem];
     [customFBLoginButton setTranslatesAutoresizingMaskIntoConstraints:NO];
     
+    [customFBLoginButton setBackgroundColor: [UIColor colorWithRed:59/255.0 green:86/255.0 blue:152/255.0 alpha:1]];
+    [customFBLoginButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    [customFBLoginButton setTitle:@"Login with Facebook" forState:UIControlStateNormal];
     
-    customFBLoginButton.backgroundColor = [UIColor blueColor];
+    [customFBLoginButton addTarget:nil
+                            action:@selector(loginWithFacebook)
+                  forControlEvents:UIControlEventTouchUpInside];
     
     [self.loginButtonsStack addArrangedSubview:customFBLoginButton];
     self.customFBLoginButton = customFBLoginButton;
     
+}
+- (void) loginWithFacebook {
+    NSLog(@"Trying to loging with Facebook");
+    
+    [[[FBSDKLoginManager alloc] init] logInWithReadPermissions:@[@"public_profile", @"email"]
+                                            fromViewController:self
+                                                       handler:^(FBSDKLoginManagerLoginResult *result, NSError *error)
+    {
+        if(error) {
+            NSLog(@"=== ERROR: %@", error.debugDescription);
+            return;
+        }
+        NSLog(@"logged with FB with Token: %@", result.token.tokenString);
+    }];
 }
 
 - (void)loginButtonDidLogOut:(FBSDKLoginButton *)loginButton {
@@ -106,15 +126,12 @@
 }
 
 - (void)onFacebookLogin {
-    NSLog(@"=== INFO: Successfully logged with Facebook: onFacebookLogin");
-    
     [[[FBSDKGraphRequest alloc] initWithGraphPath:@"me" parameters:@{@"fields":@"id, name, email, picture"}]
      startWithCompletionHandler:^(FBSDKGraphRequestConnection *connection, id result, NSError *error) {
-         if (!error) {
-             NSLog(@"=== fetched user: %@", result);
+         if (error) {
+             NSLog(@"=== ERROR fetching FB User Data: %@", error);
          }
-         
-     }];
+         NSLog(@"=== fetched user: %@", result);}];
 };
 
 @end
